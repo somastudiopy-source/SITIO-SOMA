5cac5fef7f25efc659019f5a2d91944fcbfd14a5
somastudiopy-source <soma.studio.py@gmail.com>
2026-04-18 13:08:23 -0300

    Update index.js

--- CLIENTES/CATALEYA/wa-openai/index.js
+++ CLIENTES/CATALEYA/wa-openai/index.js
@@ -8412,6 +8412,9 @@ function toYMD(dateStr) {
   let m = s.match(/^(\d{4})-(\d{2})-(\d{2})$/);
   if (m) return buildValidYMD(m[1], m[2], m[3]);
 
+  m = s.match(/^(\d{4})-(\d{2})-(\d{2})[T\s]/);
+  if (m) return buildValidYMD(m[1], m[2], m[3]);
+
   m = s.match(/^(\d{4})[\/\-](\d{1,2})[\/\-](\d{1,2})$/);
   if (m) return buildValidYMD(m[1], m[2], m[3]);
 
